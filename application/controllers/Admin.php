ea231197684c94347081d9dae9f3afca0c7affb9
reydkosasih <work.reydkosasih@gmail.com>
2026-07-15 16:24:59 +0700

    Donwload QR Room Button
    Menambahkan tombol untuk download QR ruangan survey

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -41,6 +41,18 @@ class Admin extends MY_Controller
         $this->load->view('admin/v_print_qr', $data);
     }
 
+    public function download_qr($id)
+    {
+        $room = $this->M_room->get_by_id($id);
+        if (!$room) {
+            show_404();
+            return;
+        }
+
+        $data['room'] = $room;
+        $this->load->view('admin/v_download_qr', $data);
+    }
+
     public function get_rooms()
     {
         $this->load->library('table');
@@ -65,6 +77,7 @@ class Admin extends MY_Controller
                 : '<span class="badge badge-danger">Nonaktif</span>';
 
             $action = '<a href="' . base_url('admin/print_qr/' . $row->id) . '" target="_blank" class="btn btn-sm btn-info mr-1" title="Cetak QR"><i class="fa fa-qrcode"></i></a>';
+            $action .= '<a href="' . base_url('admin/download_qr/' . $row->id) . '" target="_blank" class="btn btn-sm btn-success mr-1" title="Unduh QR"><i class="fa fa-download"></i></a>';
             $action .= '<button class="btn btn-sm btn-primary btn-edit-room mr-1" data-id="' . $row->id . '" title="Edit"><i class="fa fa-edit"></i></button>';
             $action .= '<button class="btn btn-sm btn-danger btn-delete-room" data-id="' . $row->id . '" data-name="' . htmlspecialchars($row->name) . '" title="Hapus"><i class="fa fa-trash"></i></button>';
 
